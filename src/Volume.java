160bc46fecd0b47da290e860b9ed5a87e74ce806
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-07 11:22:26 -0300

    trocando o usuario

--- src/Volume.java
+++ src/Volume.java
@@ -4,7 +4,7 @@
 // métodos da interface são métodos abstratos.
 // métodos são publicos mesmo que não esteja marcado
 // com public.
-// interface não pode instaciada porque não tem cosntrutor
+// interface não pode instaciada porque não tem cosntrutor.
 public interface Volume {
     public abstract double calcularVolume();
 }
